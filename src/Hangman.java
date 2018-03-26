89967c7fda0c13652b65ce635476855952b832f6
Kaiya <league@WTS13.attlocal.net>
2018-03-25 17:29:32 -0700

    working on it, need to work on the void checkWord

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,3 +1,5 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -5,30 +7,44 @@ import java.io.IOException;
 import java.util.Random;
 import java.util.Stack;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
-public class Hangman {
+public class Hangman implements KeyListener{
+	JFrame f = new JFrame();
+	JPanel p = new JPanel();
+	JLabel l = new JLabel();
+	JLabel guess = new JLabel();
+	Stack<String> s = new Stack <String>();
+	String currentWord;
+	String displayedWord = "";
+	char gues;
+	char under = '_';
+	String temp = "";
 	public static void main(String[] args) {
-		
+		Hangman h = new Hangman();
 	}
 
 	Hangman() {
 		
 		String ran = JOptionPane.showInputDialog("Enter a number between 1 - 2,999");
 		int rand = Integer.parseInt(ran);
-		Stack<String> s = new Stack <String>();
+
 		for (int i = 0; i < rand; i++) {
 			Random r = new Random();
 			int num = r.nextInt(3000);
 			int st = 0;
 			try {
-				BufferedReader br = new BufferedReader(new FileReader("Hangman/src/dictionary.txt"));
+				BufferedReader br = new BufferedReader(new FileReader("src/dictionary.txt"));
 				String line = br.readLine();
 				while(line != null){
 					line = br.readLine();
 					st++;
 					if(st == num) {
 						s.add(line);
+						currentWord = line;
 					}
 				}
 				
@@ -41,6 +57,60 @@ public class Hangman {
 				e.printStackTrace();
 			}
 		}
+		f.addKeyListener(this);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.add(p);
+		p.add(l);
+		p.add(guess);
+		l.setText("Guess a Letter");
+		f.setVisible(true);
+		initializeDisplayedWord();
+		f.setSize(500, 300);
+	}
+	void initializeDisplayedWord() {
+		for (int i = 0; i < currentWord.length(); i++) {
+			displayedWord += "_";
+		}
+		guess.setText(displayedWord);
+	}
+	void updateDisplayedWord(char c) {
+
+			for (int j = 0; j < currentWord.length(); j++) {
+				if (currentWord.charAt(j)==c) {
+					temp += c;
+				}
+				else {
+					temp += displayedWord.charAt(j);
+				}
+			}
+			displayedWord = temp;
+
+	}
+	void checkWord(String ch) {
+		for (int i = 0; i < ch.length(); i++) {
+			if (ch.charAt(i) == under) {
+				return;
+			}
+		}
+				currentWord = s.pop();
+	}
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		updateDisplayedWord(e.getKeyChar());
+		guess.setText(displayedWord);
+		checkWord(temp);
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 }
 
